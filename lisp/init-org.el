fd006082b7bd5e3dfed5e7d92617d985b43052fe
harumi <h.kiyama0720@gmail.com>
2023-02-17 17:09:28 +0800

    update code

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -64,7 +64,6 @@
           org-archive-location "~/org-mode/archive.org::"
           org-startup-truncated nil)
 
-
     ;; https://emacs-china.org/t/ox-hugo-auto-fill-mode-markdown/9547/4
     (defadvice org-hugo-paragraph (before org-hugo-paragraph-advice
                                           (paragraph contents info) activate)
@@ -104,13 +103,6 @@ unwanted space when exporting org-mode to hugo markdown."
 
     (setq org-tags-match-list-sublevels nil)
 
-    (add-hook 'org-mode-hook (lambda ()
-                               ;; keybinding for editing source code blocks
-                               (when (featurep 'company)
-                                 (company-mode -1))
-                               ;; keybinding for inserting code blocks
-                               (local-set-key (kbd "C-c i s")
-                                              'harumi/org-insert-src-block)))
     (require 'ox-publish)
     (add-to-list 'org-latex-classes '("ctexart" "\\documentclass[11pt]{ctexart}
                                         [NO-DEFAULT-PACKAGES]
@@ -176,49 +168,12 @@ unwanted space when exporting org-mode to hugo markdown."
 
     ;;reset subtask
     (setq org-default-properties (cons "RESET_SUBTASKS" org-default-properties))
-
-    ;; (add-hook 'org-after-todo-state-change-hook 'org-subtask-reset)
     (setq org-return-follows-link t)
-
-    (setq org-plantuml-jar-path
-          (expand-file-name "~/.doom.d/plantuml.jar"))
-    (setq org-ditaa-jar-path "~/.doom.d/ditaa.jar")
 
     (require 'org-protocol)
     ;; https://chenzaichun.github.io/post/2021-10-04-org-roam-research-start/
-
-
     ;; https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#configuring-doom
-    ;; (org-babel-do-load-languages
-    ;;  'org-babel-load-languages
-    ;;  '((perl . t)
-    ;;    (ruby . t)
-    ;;    (shell . t)
-    ;;    (dot . t)
-    ;;    (typescript . t)
-    ;;    (js . t)
-    ;;    (latex .t)
-    ;;    (python . t)
-    ;;    (emacs-lisp . t)
-    ;;    (plantuml . t)
-    ;;    (C . t)
-    ;;    (ditaa . t)))
-
-    (progn
-
-      (use-package cal-china-x
-        :ensure t
-        :demand t)
-      (setq mark-holidays-in-calendar t)
-      (setq cal-china-x-important-holidays cal-china-x-chinese-holidays)
-      (setq cal-china-x-general-holidays '((holiday-lunar 1 15 "元宵节")))
-      (setq calendar-holidays
-            (append cal-china-x-important-holidays
-                    cal-china-x-general-holidays)))
     (require 'ox-md nil t)
-
-    ;; define the refile targets
-
     ;; C-n for the next org agenda item
     (define-key org-agenda-mode-map (kbd "C-p") 'org-agenda-previous-item)
 
@@ -227,261 +182,13 @@ unwanted space when exporting org-mode to hugo markdown."
       (define-key org-agenda-mode-map (kbd "P") 'org-pomodoro)
       ;; 默认显示节假日
       (setq org-agenda-include-diary t))
-    ;; the %i would copy the selected text into the template
     ;;http://www.howardism.org/Technical/Emacs/journaling-org.html
-    ;;add multi-file journal
-
-    (with-eval-after-load 'org-capture
-      (defun org-hugo-new-subtree-post-capture-template ()
-        "Returns `org-capture' template string for new Hugo post.
-See `org-capture-templates' for more information."
-        (let* ((title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
-               (fname (org-hugo-slug title)))
-          (mapconcat #'identity
-                     `(
-                       ,(concat "* TODO " title)
-                       ":PROPERTIES:"
-                       ,(concat ":EXPORT_FILE_NAME: " fname)
-                       ":END:"
-                       "\n\n")          ;Place the cursor here finally
-                     "\n")))
-
-      (add-to-list 'org-capture-templates
-                   '("h"                ;`org-capture' binding + h
-                     "Hugo post"
-                     entry
-                     ;; It is assumed that below file is present in `org-directory'
-                     ;; and that it has a "Blog Ideas" heading. It can even be a
-                     ;; symlink pointing to the actual location of all-posts.org!
-                     (file+headline org-agenda-file-blogposts "Blog Ideas")
-                     (function org-hugo-new-subtree-post-capture-template))))
-
-
-    (add-to-list 'org-agenda-custom-commands
-                 '("r" "Daily Agenda Review"
-                   ((agenda "" ((org-agenda-overriding-header "今日记录")
-                                (org-agenda-span 'day)
-                                (org-agenda-show-log 'clockcheck)
-                                (org-agenda-start-with-log-mode nil)
-                                (org-agenda-log-mode-items '(closed clock state))
-                                (org-agenda-clockreport-mode t))))))
-
-    ;; hack for org headline toc
-    (defun org-html-headline (headline contents info)
-      "Transcode a HEADLINE element from Org to HTML.
-CONTENTS holds the contents of the headline.  INFO is a plist
-holding contextual information."
-      (unless (org-element-property :footnote-section-p headline)
-        (let* ((numberedp (org-export-numbered-headline-p headline info))
-               (numbers (org-export-get-headline-number headline info))
-               (section-number (and numbers
-                                    (mapconcat #'number-to-string numbers "-")))
-               (level (+ (org-export-get-relative-level headline info)
-                         (1- (plist-get info :html-toplevel-hlevel))))
-               (todo (and (plist-get info :with-todo-keywords)
-                          (let ((todo (org-element-property :todo-keyword headline)))
-                            (and todo (org-export-data todo info)))))
-               (todo-type (and todo (org-element-property :todo-type headline)))
-               (priority (and (plist-get info :with-priority)
-                              (org-element-property :priority headline)))
-               (text (org-export-data (org-element-property :title headline) info))
-               (tags (and (plist-get info :with-tags)
-                          (org-export-get-tags headline info)))
-               (full-text (funcall (plist-get info :html-format-headline-function)
-                                   todo todo-type priority text tags info))
-               (contents (or contents ""))
-               (ids (delq nil
-                          (list (org-element-property :CUSTOM_ID headline)
-                                (org-export-get-reference headline info)
-                                (org-element-property :ID headline))))
-               (preferred-id (car ids))
-               (extra-ids
-                (mapconcat
-                 (lambda (id)
-                   (org-html--anchor
-                    (if (org-uuidgen-p id) (concat "ID-" id) id)
-                    nil nil info))
-                 (cdr ids) "")))
-          (if (org-export-low-level-p headline info)
-              ;; This is a deep sub-tree: export it as a list item.
-              (let* ((type (if numberedp 'ordered 'unordered))
-                     (itemized-body
-                      (org-html-format-list-item
-                       contents type nil info nil
-                       (concat (org-html--anchor preferred-id nil nil info)
-                               extra-ids
-                               full-text))))
-                (concat (and (org-export-first-sibling-p headline info)
-                             (org-html-begin-plain-list type))
-                        itemized-body
-                        (and (org-export-last-sibling-p headline info)
-                             (org-html-end-plain-list type))))
-            (let ((extra-class (org-element-property :HTML_CONTAINER_CLASS headline))
-                  (first-content (car (org-element-contents headline))))
-              ;; Standard headline.  Export it as a section.
-              (format "<%s id=\"%s\" class=\"%s\">%s%s</%s>\n"
-                      (org-html--container headline info)
-                      (org-export-get-reference headline info)
-                      (concat (format "outline-%d" level)
-                              (and extra-class " ")
-                              extra-class)
-                      (format "\n<h%d id=\"%s\">%s%s</h%d>\n"
-                              level
-                              preferred-id
-                              extra-ids
-                              (concat
-                               (and numberedp
-                                    (format
-                                     "<span class=\"section-number-%d\">%s</span> "
-                                     level
-                                     (mapconcat #'number-to-string numbers ".")))
-                               full-text)
-                              level)
-                      ;; When there is no section, pretend there is an
-                      ;; empty one to get the correct <div
-                      ;; class="outline-...> which is needed by
-                      ;; `org-info.js'.
-                      (if (eq (org-element-type first-content) 'section) contents
-                        (concat (org-html-section first-content "" info) contents))
-                      (org-html--container headline info)))))))))
-
-
-(use-package org-roam
-  :init
-  (defun jethro/org-capture-slipbox ()
-    (interactive)
-    (org-capture nil "s"))
-  :custom
-  (org-roam-directory (file-truename org-directory))
-  :config
-    (cl-defmethod org-roam-node-type ((node org-roam-node))
-    "Return the TYPE of NODE."
-    (condition-case nil
-        (file-name-nondirectory
-         (directory-file-name
-          (file-name-directory
-           (file-relative-name (org-roam-node-file node) org-roam-directory))))
-      (error "")))
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-    (setq org-roam-node-display-template (concat "${type:10} ${title:20} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  (setq org-roam-dailies-directory "daily/")
-
-  (setq org-roam-capture-templates
-      '(("m" "main" plain
-         "%?"
-         :if-new (file+head "main/${slug}.org"
-                            "#+title: ${title}\n")
-         :immediate-finish t
-         :unnarrowed t)
-        ("r" "reference" plain "%?"
-         :if-new
-         (file+head "reference/${title}.org" "#+title: ${title}\n")
-         :immediate-finish t
-         :unnarrowed t)
-        ("a" "article" plain "%?"
-         :if-new
-         (file+head "articles/${title}.org" "#+title: ${title}\n#+filetags: :article:\n")
-         :immediate-finish t
-         :unnarrowed t)))
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol)
-  :bind (("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n r" . org-roam-node-random)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today)
-         (:map org-mode-map
-               (("C-c n i" . org-roam-node-insert)
-                ("C-c n o" . org-id-get-create)
-                ("C-c n t" . org-roam-tag-add)
-                ("C-c n E" . org-roam-extract-subtree)
-                ("C-c n a" . org-roam-alias-add)
-                ("C-c n l" . org-roam-buffer-toggle)))))
+    ))
 
 (use-package org-contrib
   :pin nongnu
   :init
   :after org
   (require 'org-checklist))
-
-(use-package org-roam-ui
-  :ensure t
-  :commands (org-roam-ui-mode)
-  :after org
-  )
-
-(use-package consult-org-roam
-  :ensure nil
-  :init
-  :commands (consult-org-roam-forward-links)
-  :custom
-  (consult-org-roam-grep-func #'consult-ripgrep)
-  :config
-  ;; Eventually suppress previewing for certain functions
-  (consult-customize
-   consult-org-roam-forward-links
-   :preview-key (kbd "s-."))
-  :bind
-  ("C-c n e" . consult-org-roam-forward-links)
-  ("C-c n b" . consult-org-roam-backlinks)
-  ("C-c n s" . consult-org-roam-search))
-
-(use-package org-download
-  ;; :ensure-system-package (pngpaste . "brew install pngpaste")
-  :ensure t
-  :demand t
-  :after org
-  :config
-  (add-hook 'dired-mode-hook 'org-download-enable)
-  (setq-default org-download-heading-lvl nil
-                org-download-image-dir "./img"
-                ;; org-download-screenshot-method "screencapture -i %s"
-                org-download-screenshot-method "pngpaste %s"
-                org-download-screenshot-file (expand-file-name "screenshot.jpg" temporary-file-directory)))
-
-(defun dw/org-present-prepare-slide ()
-  (org-overview)
-  (org-show-entry)
-  (org-show-children))
-
-(defun dw/org-present-hook ()
-  (setq-local face-remapping-alist '((default (:height 1.5) variable-pitch)
-                                     (header-line (:height 4.5) variable-pitch)
-                                     (org-code (:height 1.55) org-code)
-                                     (org-verbatim (:height 1.55) org-verbatim)
-                                     (org-block (:height 1.25) org-block)
-                                     (org-block-begin-line (:height 0.7) org-block)))
-  (setq header-line-format " ")
-  (org-display-inline-images)
-  (dw/org-present-prepare-slide)
-  (visual-fill-column-mode 1)
-  (visual-line-mode 1))
-
-(defun dw/org-present-quit-hook ()
-  (setq-local face-remapping-alist '((default variable-pitch default)))
-  (setq header-line-format nil)
-  (org-present-small)
-  (org-remove-inline-images)
-  (visual-fill-column-mode 0)
-  (visual-line-mode 0))
-
-(defun dw/org-present-prev ()
-  (interactive)
-  (org-present-prev)
-  (dw/org-present-prepare-slide))
-
-(defun dw/org-present-next ()
-  (interactive)
-  (org-present-next)
-  (dw/org-present-prepare-slide))
-
-(use-package org-present
-  :bind (:map org-present-mode-keymap
-         ("C-c C-j" . dw/org-present-next)
-         ("C-c C-k" . dw/org-present-prev))
-  :hook ((org-present-mode . dw/org-present-hook)
-         (org-present-mode-quit . dw/org-present-quit-hook)))
 
 (provide 'init-org)
