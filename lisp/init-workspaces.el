81aa2b8ac221fc18b257537bd4558acc385a4f65
harumi <h.kiyama0720@gmail.com>
2023-09-10 15:55:52 +0800

    update emacs workspaces config

--- lisp/init-workspaces.el
+++ lisp/init-workspaces.el
@@ -1,7 +1,45 @@
 ;;; init-workspaces.el -*- lexical-binding: t no-byte-compile: t -*-
 
-(use-package bufler
+(use-package tabspaces
+  ;; use this next line only if you also use straight, otherwise ignore it.
+  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup.
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  ;; sessions
+  (tabspaces-session t)
+  
+  (with-eval-after-load 'consult
+    ;; hide full buffer list (still available with "b" prefix)
+    (consult-customize consult--source-buffer :hidden t :default nil)
+    ;; set consult-workspace buffer list
+    (defvar consult--source-workspace
+      (list :name "Workspace Buffers"
+            :narrow ?w
+            :history 'buffer-name-history
+            :category 'buffer
+            :state #'consult--buffer-state
+            :default t
+            :items (lambda () (consult--buffer-query
+                               :predicate #'tabspaces--local-buffer-p
+                               :sort 'visibility
+                               :as #'buffer-name)))
+
+      "Set workspace buffer list for consult-buffer.")
+    (add-to-list 'consult-buffer-sources 'consult--source-workspace)))
+
+(use-package ibuffer-vc
   :config
-  (bufler-tabs-mode))
+  (add-hook 'ibuffer-hook
+            (lambda ()
+              (ibuffer-vc-set-filter-groups-by-vc-root)
+              (unless (eq ibuffer-sorting-mode 'alphabetic)
+                (ibuffer-do-sort-by-alphabetic)))))
+
+
 
 (provide 'init-workspaces)
