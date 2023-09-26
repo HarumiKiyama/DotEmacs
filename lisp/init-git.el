2102837b7c13b71ffe97d0316071bf7248d1ddb2
harumi <h.kiyama0720@gmail.com>
2023-09-26 22:11:16 +0800

    update

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,39 +2,38 @@
 
 (use-package magit
   :commands (magit-status)
+  :hook (magit-diff-visit-file . (lambda () (when smerge-mode  (hydra-smerge/body))))
   :init
   (setq magit-refs-show-commit-count 'all
-        magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
-  )
+        magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
 
 (use-package smerge-mode
   :ensure nil
   :pretty-hydra
   (hydra-smerge (:color pink :quit-key "q" :pre (smerge-mode 1)
-                           :post (smerge-auto-leave) :hint nil)
-    ("Move"
-     (("n" smerge-next "next")
-      ("p" smerge-prev "prev"))
+                        :post (smerge-auto-leave) :hint nil)
+                ("Move"
+                 (("n" smerge-next "next")
+                  ("p" smerge-prev "prev"))
 
-     "Keep"
-     (("b" smerge-keep-base "base")
-      ("u" smerge-keep-upper "upper")
-      ("l" smerge-keep-lower "lower")
-      ("a" smerge-keep-all "all")
-      ("RET" smerge-keep-current "current"))
+                 "Keep"
+                 (("b" smerge-keep-base "base")
+                  ("u" smerge-keep-upper "upper")
+                  ("l" smerge-keep-lower "lower")
+                  ("a" smerge-keep-all "all")
+                  ("RET" smerge-keep-current "current"))
 
+                 "Diff"
+                 (("<" smerge-diff-base-upper "upper/base")
+                  ("=" smerge-diff-upper-lower "upper/lower")
+                  (">" smerge-diff-base-lower "base/lower")
+                  ("R" smerge-refine "refine"))
 
-     "Diff"
-     (("<" smerge-diff-base-upper "upper/base")
-      ("=" smerge-diff-upper-lower "upper/lower")
-      (">" smerge-diff-base-lower "base/lower")
-      ("R" smerge-refine "refine"))
-
-     "Other"
-     (("C" smerge-combine-with-next "Combine")
-      ("r" smerge-resolve "resolve")
-      ("k" smerge-kill-current "kill current")))))
+                 "Other"
+                 (("C" smerge-combine-with-next "Combine")
+                  ("r" smerge-resolve "resolve")
+                  ("k" smerge-kill-current "kill current")))))
 
 
 (use-package git-timemachine)
--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -27,22 +27,21 @@
   :config
   (setq mail-user-agent 'gnus-user-agent)
   (setq org-msg-options "html-postamble:nil H:5 num:nil ^:{} toc:nil author:nil email:nil \\n:t"
-	    org-msg-startup "hidestars indent inlineimages"
-	    org-msg-greeting-fmt "\nHi%s,\n\n"
-	    org-msg-recipient-names '(("jeremy.compostella@gmail.com" . "Jérémy"))
-	    org-msg-greeting-name-limit 3
-	    org-msg-default-alternatives '((new		. (text html))
-				                       (reply-to-html	. (text html))
-				                       (reply-to-text	. (text)))
-	    org-msg-convert-citation t
-	    org-msg-signature "
+        org-msg-startup "hidestars indent inlineimages"
+        org-msg-greeting-fmt "\nHi%s,\n\n"
+        org-msg-recipient-names '(("jeremy.compostella@gmail.com" . "Jérémy"))
+        org-msg-greeting-name-limit 3
+        org-msg-default-alternatives '((new text html)
+                                       (reply-to-html text html)
+                                       (reply-to-text text))
+        org-msg-convert-citation t
+        org-msg-signature "
  Regards,
  #+begin_signature
  --
  *harumi*
  #+end_signature")
-  (org-msg-mode)
-  )
+  (org-msg-mode))
 
 (use-package org-contacts)
 
